66a51c872c48e6dc1fa29b428b11a3473fed04de
tyc2020 <tyc2020@163.com>
2020-04-27 09:40:20 +0800

    构建项目并配置全局样式、element

---
+++ vue/src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    redirect: "login"
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: () => import("../views/Login/index.vue")
+  }
+];
+
+const router = new VueRouter({
+  routes
+});
+
+export default router;
---
+++ vue/src/views/Login/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="login">
+    <el-button>
+      经典    
+    </el-button>
+
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+<style lang="scss" scoped>
+  #login{
+    background-color:#344a5f;
+    height:100vh
+  }
+</style>
